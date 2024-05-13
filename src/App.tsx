455dbb9fa4d9a83505cefc93a056ae20f3886815
Rudolph Christian Razul <razulchristian@gmail.com>
2024-05-13 23:56:36 +0800

    Added new fonts: OverusedGrotesk and Satoshi

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import './App.css';
-
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="flex h-screen w-full flex-col items-center justify-center">
+        <h1 className="font-overused text-7xl font-bold">CHRISTIAN RAZUL</h1>
+        <p className="font-overused text-xl">
+          A freelance front-end developer and web designer helping startups all
+          around the world gain their unfair advantage.
+        </p>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './index.html',
-    './src/**/*.{js,jsx,ts,tsx}', // Adjust this line if you're using TypeScript
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust this line if you're using TypeScript
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        overused: ["OverusedGrotesk", "sans-serif"],
+        satoshi: ["Satoshi", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
