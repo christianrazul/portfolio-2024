a363c5a5c89595641b8aceedf98fb8e4f0cdf2ac
Rudolph Christian Razul <razulchristian@gmail.com>
2024-05-14 16:49:19 +0800

    Add some more theme options for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
@@ -17,7 +17,33 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily: {
+        overused: ["OverusedGrotesk", "sans-serif"],
+        satoshi: ["Satoshi", "sans-serif"],
+      },
+      letterSpacing: {
+        headings: "-0.03em",
+      },
+      lineHeight: {
+        extratight: "0.90",
+      },
       colors: {
+        transparent: "transparent",
+        "primary-200": "#F2F2F2",
+        "primary-300": "#E6E6E6",
+        "primary-400": "#D9D9D9",
+        "secondary-100": "#FAFAF9",
+        "secondary-200": "#E8E8E3",
+        "secondary-300": "#DDDDD5",
+        "secondary-400": "#D1D1C7",
+        "secondary-500": "#AEAE9D",
+        "secondary-600": "#8C8C73",
+        "secondary-700": "#70705C",
+        "accent-400": "#0E0E0C",
+        "accent-300": "#262626",
+        "accent-200": "#4D4D4D",
+        "accent-100": "#666666",
+
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
@@ -52,6 +78,9 @@ module.exports = {
           foreground: "hsl(var(--card-foreground))",
         },
       },
+      screens: {
+        "3xl": "1600px",
+      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
@@ -74,4 +103,4 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
